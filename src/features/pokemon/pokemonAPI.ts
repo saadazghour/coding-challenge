9fae9bbafd70995dd8e989a700064cf7f7f1fda0
saadazghour <saad.azghour@gmail.com>
2024-01-28 19:41:28 +0100

    feat: Add error handling to fetchPokemon async request
    - Add the fetchPokemon function to handle and report errors during API requests. This ensures the application can respond appropriately to issues such as network failures or unexpected API changes.

---
+++ src/features/pokemon/pokemonAPI.ts
@@ -0,0 +1,33 @@
+// fetching a subset of items from a larger list, which is a common operation in pagination.
+
+// Here's a breakdown of the concept:
+
+// amount: This represents the number of items you want to display on one page.
+// page: This is the index of the current page. It's usually zero-indexed, meaning the first page is 0, the second page is 1, and so on.
+// offset: This represents the number of items to skip before starting to fetch the data. It's calculated by multiplying the page number by the amount of items per page.
+
+// For example, if you're on page 1 (page = 0 since it's zero-indexed) and you want 20 items per page (amount = 20), the offset will be 0 * 20 = 0, so you start from the first item. If you're on page 2 (page = 1), the offset will be 1 * 20 = 20, so you skip the first 20 items and start fetching from the 21st item. This pattern allows you to fetch the correct subset of items for any given page number.
+
+export function fetchPokemon(amount = 20, page = 0) {
+  const offset = page * amount;
+
+  return new Promise<{ data: any; error?: Error }>((resolve, reject) =>
+    setTimeout(async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${amount}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        resolve({ data: data.results });
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        reject(error);
+      }
+    }, 500)
+  );
+}
