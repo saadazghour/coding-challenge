87ad2bf0849fe5a48300b2fbff3eb5f335c81c64
saadazghour <saad.azghour@gmail.com>
2024-01-30 10:57:22 +0100

    Implement Infinite Scrolling for Pokemon List Fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { RootState } from "./app/store";
+import { pokemonListAsync } from "./features/pokemon/pokemonSlice";
+import { useAppDispatch } from "./app/hooks";
+import { Pokedex } from "./components/Pokedex";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const pokemonList = useSelector((state: RootState) => state.pokemon.pokemon);
+  const status = useSelector((state: RootState) => state.pokemon.status);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(0); // Initial page number
+
+  useEffect(() => {
+    // Fetch initial data when component mounts
+    if (currentPage > 0) {
+      dispatch(pokemonListAsync({ amount: 20, page: currentPage }));
+    }
+  }, [currentPage, dispatch]);
+
+  // console.log(pokemonList, status);
+
+  const fetchNextPage = () => {
+    if (status !== "loading") {
+      // console.log("Fetching next page");
+      setCurrentPage((prevPage) => prevPage + 1);
+      dispatch(pokemonListAsync({ amount: 20, page: currentPage + 1 }));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Pokedex
+        setModal={setModalOpen}
+        // setPokemonData={handleSetPokemonData}
+        pokemonList={pokemonList}
+        fetchNextPage={fetchNextPage}
+      />
     </div>
   );
 }
