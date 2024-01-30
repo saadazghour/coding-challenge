61615051e212a972a8322ef202dfab463472c6d1
saadazghour <saad.azghour@gmail.com>
2024-01-30 10:51:56 +0100

    refactor Pokedex && Add Infinite Scroll.

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,25 +1,44 @@
 import { PokemonCard } from "./PokemonCard";
 import { Pokemon } from "../types/Pokemon";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 type PokedexProps = {
   pokemonList: Pokemon[];
   setModal: (value: boolean) => void;
+  fetchNextPage: () => void;
 };
 
-export const Pokedex = (props: PokedexProps) => {
+export const Pokedex = ({
+  pokemonList,
+  setModal,
+  fetchNextPage,
+}: PokedexProps) => {
+  // The useInView hook makes it easy to monitor the inView state of your components. Call the useInView hook with the (optional) options you need. It will return an array containing a ref, the inView status and the current entry. Assign the ref to the DOM element you want to monitor, and the hook will report the status.
+  
+
+  const { ref, inView } = useInView();
+
+  // Call fetchNextPage when the bottom of the list is in view
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
   return (
     <div className="mb-36">
       <div className="grid gap-4 row-gap-12 px-6 py-2 m-auto column-gap-2 md:grid-cols-3">
-        {props.pokemonList.map((pokemon) => (
-          <PokemonCard
-            key={pokemon.id}
-            pokemon={pokemon}
-            setModal={props.setModal}
-          />
+        {pokemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} setModal={setModal} />
         ))}
       </div>
-      TODO:
-      {/* Infinite scroll and Button Container logic */}
+      <div
+        ref={ref}
+        className="flex items-center rounded-md justify-center w-[50%] px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600  hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 my-8 mx-auto"
+      >
+        Loading...
+      </div>
     </div>
   );
 };
