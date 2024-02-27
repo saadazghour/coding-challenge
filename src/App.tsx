af49b75b374aff54f8a6900c3a750425a77faf92
saadazghour <saad.azghour@gmail.com>
2024-02-27 12:57:49 +0100

    fix: pagination should be handled with redux and not at the root level of the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useSelector } from "react-redux";
 import { RootState } from "./app/store";
-import { pokemonListAsync } from "./features/pokemon/pokemonSlice";
+import { pokemonListAsync, nextPage } from "./features/pokemon/pokemonSlice";
 import { useAppDispatch } from "./app/hooks";
 import { Pokedex } from "./components/Pokedex";
 
 function App() {
   const dispatch = useAppDispatch();
-  const pokemonList = useSelector((state: RootState) => state.pokemon.pokemon);
-  const status = useSelector((state: RootState) => state.pokemon.status);
   const [modalOpen, setModalOpen] = useState(false);
 
-  const [currentPage, setCurrentPage] = useState(0); // Initial page number
+  const { pokemon, status, page } = useSelector(
+    (state: RootState) => state.pokemon
+  );
 
   useEffect(() => {
-    // Fetch initial data when component mounts
-    if (currentPage > 0) {
-      dispatch(pokemonListAsync({ amount: 20, page: currentPage }));
-    }
-  }, [currentPage, dispatch]);
+    // Fetch initial data when component mounts or page changes
+    dispatch(pokemonListAsync({ amount: 20, page }));
+  }, [page, dispatch]);
 
   // console.log(pokemonList, status);
 
   const fetchNextPage = () => {
     if (status !== "loading") {
-      // console.log("Fetching next page");
-      setCurrentPage((prevPage) => prevPage + 1);
-      dispatch(pokemonListAsync({ amount: 20, page: currentPage + 1 }));
+      // Increment the page number in the Redux store
+      dispatch(nextPage());
     }
   };
 
@@ -36,7 +33,7 @@ function App() {
       <Pokedex
         modalOpen={modalOpen}
         setModal={setModalOpen}
-        pokemonList={pokemonList}
+        pokemonList={pokemon}
         fetchNextPage={fetchNextPage}
       />
     </div>
